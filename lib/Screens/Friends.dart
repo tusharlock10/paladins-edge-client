a8fbdf04de0abaf3957c5394c4db4318be53ad2e
Tushar Jain <tusharlock10@gmail.com>
2021-10-16 03:04:40 +0530

    feat: added avatar in friends

--- lib/Screens/Friends.dart
+++ lib/Screens/Friends.dart
@@ -205,6 +205,10 @@ class _PlayerDetailState extends State<Friends> {
                                 style: theme.textTheme.headline3,
                               ),
                               Text(
+                                'Level ${this.selectedFriend!.level}',
+                                style: theme.textTheme.bodyText2,
+                              ),
+                              Text(
                                 '${this.selectedFriend!.platform}',
                                 style: theme.textTheme.bodyText1,
                               ),
@@ -274,30 +278,57 @@ class _PlayerDetailState extends State<Friends> {
       height: 80,
       child: Card(
         elevation: 7,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${friend.name}',
-                    style: theme.textTheme.headline3,
-                  ),
-                  Text('${friend.platform}', style: theme.textTheme.bodyText1),
-                ],
+        child: Padding(
+          padding: EdgeInsets.only(left: 15),
+          child: Row(
+            children: [
+              friend.avatarUrl != null
+                  ? Widgets.ElevatedAvatar(
+                      size: 24,
+                      borderRadius: 5,
+                      imageUrl: friend.avatarUrl!,
+                    )
+                  : SizedBox(),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${friend.name}',
+                      style: theme.textTheme.headline3?.copyWith(fontSize: 16),
+                    ),
+                    friend.title != null
+                        ? Text(
+                            '${friend.title}',
+                            style: theme.textTheme.bodyText2
+                                ?.copyWith(fontSize: 12),
+                          )
+                        : SizedBox(),
+                    Text('${friend.platform}',
+                        style:
+                            theme.textTheme.bodyText1?.copyWith(fontSize: 10)),
+                  ],
+                ),
               ),
-            ),
-            isFavourite
-                ? Container(
-                    color: Colors.yellow,
-                    height: 80,
-                    width: 3,
-                  )
-                : SizedBox()
-          ],
+              Expanded(
+                child: SizedBox(),
+              ),
+              friend.ranked.rank != 0 && friend.ranked.rankIconUrl != null
+                  ? Widgets.FastImage(
+                      height: 36,
+                      width: 36,
+                      imageUrl: friend.ranked.rankIconUrl!,
+                    )
+                  : SizedBox(),
+              Container(
+                color: isFavourite ? Colors.yellow : Colors.transparent,
+                height: 80,
+                width: 3,
+              )
+            ],
+          ),
         ),
       ),
     );
