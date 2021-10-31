d8bd46a620e6d6b8697480da0df5579be5d71ecb
Tushar Jain <tusharlock10@gmail.com>
2021-11-01 03:26:36 +0530

    feat: implemented local notifications

--- lib/utilities/messaging.dart
+++ lib/utilities/messaging.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart' as http;
 import 'package:overlay_support/overlay_support.dart';
 import 'package:paladinsedge/app_theme.dart' as app_theme;
 import 'package:paladinsedge/constants.dart' as constants;
+import 'package:paladinsedge/widgets/index.dart' as widgets;
+import 'package:path_provider/path_provider.dart';
 
 abstract class Messaging {
+  static final flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
   static FirebaseMessaging? messaging;
   static NotificationSettings? settings;
 
@@ -26,23 +34,105 @@ abstract class Messaging {
     return token;
   }
 
-  static Future<void> _backgroundMessageHandler(RemoteMessage message) async {}
+  static Future<void> _backgroundMessageHandler(RemoteMessage? message) async {
+    // http.get(Uri.parse(constants.Env.baseUrl));
+    if (message == null) {
+      createNewNotif(
+        title: "No message",
+        body: "getInitialMessage",
+      );
+      return;
+    }
+
+    final data =
+        message.data.map((key, value) => MapEntry(key, value?.toString()));
+    debugPrint(data.toString());
+
+    createNewNotif(
+      imageUrl: data['imageUrl'],
+      title: data['title'],
+      body: data['body'],
+    );
+  }
+
+  static Future<FilePathAndroidBitmap> _getFileAndroidBitmap(String url) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String filePath = '${directory.path}/notifLargeIcon';
+    final http.Response response = await http.get(Uri.parse(url));
+    final File file = File(filePath);
+    await file.writeAsBytes(response.bodyBytes);
+    return FilePathAndroidBitmap(filePath);
+  }
 
   static void onMessage() {
     // register in main() before runApp()
+
     FirebaseMessaging.onMessage.listen((message) {
+      final data =
+          message.data.map((key, value) => MapEntry(key, value?.toString()));
+      final imageUrl = data['imageUrl'];
+      final title = data['title'];
+      final body = data['body'];
+
+      if (imageUrl == null) return;
+
       HapticFeedback.vibrate();
       showSimpleNotification(
-        Text("${message.notification?.title}"),
-        trailing: Image.network(message.notification!.android!.imageUrl!),
-        duration: const Duration(seconds: 6),
+        Column(
+          children: [Text("$title"), Text("$body")],
+        ),
+        trailing: widgets.ElevatedAvatar(
+          size: 24,
+          borderRadius: 5,
+          imageUrl: imageUrl,
+        ),
+        duration: const Duration(seconds: 5),
         background: app_theme.themeMaterialColor,
       );
     });
   }
 
-  static void onBackgroundMessage() {
+  static void onBackgroundMessage() async {
     // register in main() before runApp()
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then(_backgroundMessageHandler);
     FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
+  }
+
+  static void createNewNotif({
+    String? title,
+    String? body,
+    String? imageUrl,
+  }) async {
+    final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      constants.NotificationChannels.friends,
+      constants.NotificationChannels.friends,
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: constants.NotificationChannels.friends,
+      largeIcon:
+          imageUrl != null ? await _getFileAndroidBitmap(imageUrl) : null,
+    );
+    final platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+    );
+    flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+    );
+  }
+
+  static void registerLocalNotification() async {
+    const initializationSettingsAndroid =
+        AndroidInitializationSettings('ic_stat_ic_notification');
+    const initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: (event) {
+      debugPrint('Event : $event');
+    });
   }
 }
