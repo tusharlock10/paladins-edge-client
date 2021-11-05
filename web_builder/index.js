b25a075267ae9f56022aaccc10d72c7e838dc472
Tushar Jain <tusharlock10@gmail.com>
2021-11-05 05:56:26 +0530

    update

--- web_builder/index.js
+++ web_builder/index.js
@@ -4,6 +4,14 @@ const { S3 } = require('@aws-sdk/client-s3');
 const fs = require('fs');
 const { PE_PRIVATE_AWS_ACCESS_KEY_ID, PE_EBS_AWS_SECRET_ACCESS_KEY, AWS_REGION, ENV_FILE, PE_PRIVATE_AWS_BUCKET_NAME } = process.env;
 
+const streamToString = async (stream) => {
+  return await new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on('data', (chunk) => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')));
+  });
+}
 
 const getCredentials = async () => {
   const client = new S3({
@@ -17,7 +25,9 @@ const getCredentials = async () => {
     Key: `credentials/client/${ENV_FILE}`
   });
 
-  fs.writeFileSync("paladins-edge.env", response.Body)
+  const data = await streamToString(response.Body)
+
+  fs.writeFileSync("paladins-edge.env", data)
 };
 
 getCredentials()
