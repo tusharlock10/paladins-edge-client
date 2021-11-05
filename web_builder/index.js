2181a4f5d7d47f45eb318620e0dbde334eb0b6a4
Tushar Jain <tusharlock10@gmail.com>
2021-11-06 04:28:36 +0530

    fix: corrected env name

--- web_builder/index.js
+++ web_builder/index.js
@@ -2,7 +2,7 @@
 
 const { S3 } = require('@aws-sdk/client-s3');
 const fs = require('fs');
-const { PE_PRIVATE_AWS_ACCESS_KEY_ID, PE_EBS_AWS_SECRET_ACCESS_KEY, AWS_REGION, ENV_FILE, PE_PRIVATE_AWS_BUCKET_NAME } = process.env;
+const { PE_PRIVATE_AWS_ACCESS_KEY_ID, PE_PRIVATE_AWS_SECRET_ACCESS_KEY, AWS_REGION, ENV_FILE, PE_PRIVATE_AWS_S3_BUCKET_NAME } = process.env;
 
 const streamToString = async (stream) => {
   return await new Promise((resolve, reject) => {
@@ -17,11 +17,11 @@ const getCredentials = async () => {
   const client = new S3({
     region: AWS_REGION, credentials: {
       accessKeyId: PE_PRIVATE_AWS_ACCESS_KEY_ID,
-      secretAccessKey: PE_EBS_AWS_SECRET_ACCESS_KEY
+      secretAccessKey: PE_PRIVATE_AWS_SECRET_ACCESS_KEY
     }
   });
   const response = await client.getObject({
-    Bucket: PE_PRIVATE_AWS_BUCKET_NAME,
+    Bucket: PE_PRIVATE_AWS_S3_BUCKET_NAME,
     Key: `credentials/client/${ENV_FILE}`
   });
 
