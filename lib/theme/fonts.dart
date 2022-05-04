ee9419c6ed9bdc9adaf7bcc9826fef32a05b674f
Tushar Jain <tusharlock10@gmail.com>
2022-05-04 23:45:37 +0530

    feat(loadout): add functionality to view other player loadouts
    User can go to Player Champions screen, from there he can visit other player loadouts
    
    resolves #275

---
+++ lib/theme/fonts.dart
@@ -0,0 +1,8 @@
+import "package:google_fonts/google_fonts.dart";
+
+abstract class Fonts {
+  static final primary = GoogleFonts.poppins().fontFamily;
+  static final primaryAccent = GoogleFonts.manrope().fontFamily;
+  static final secondary = GoogleFonts.raleway().fontFamily;
+  static final secondaryAccent = GoogleFonts.montserrat().fontFamily;
+}
