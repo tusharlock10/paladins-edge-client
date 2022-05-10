0540bf8f6864b0708f4ff9198d3d8ba2665d7c99
Tushar Jain <tusharlock10@gmail.com>
2022-05-11 04:14:56 +0530

    ci(env): add context env to switch between prod and stage deploys

--- web_builder/index.js
+++ web_builder/index.js
@@ -2,7 +2,18 @@
 
 const { S3 } = require('@aws-sdk/client-s3');
 const fs = require('fs');
-const { PE_PRIVATE_AWS_ACCESS_KEY_ID, PE_PRIVATE_AWS_SECRET_ACCESS_KEY, PE_AWS_REGION, ENV_FILE, PE_PRIVATE_AWS_S3_BUCKET_NAME } = process.env;
+const {
+  PROD_ENV_FILE,
+  STAGE_ENV_FILE,
+  PE_PRIVATE_AWS_ACCESS_KEY_ID,
+  PE_PRIVATE_AWS_SECRET_ACCESS_KEY,
+  PE_AWS_REGION,
+  PE_PRIVATE_AWS_S3_BUCKET_NAME,
+
+  CONTEXT
+} = process.env;
+
+const ENV_FILE = CONTEXT === "production" ? PROD_ENV_FILE : STAGE_ENV_FILE;
 
 const streamToString = async (stream) => {
   return await new Promise((resolve, reject) => {
@@ -11,7 +22,7 @@ const streamToString = async (stream) => {
     stream.on('error', reject);
     stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')));
   });
-}
+};
 
 const getCredentials = async () => {
   const client = new S3({
@@ -25,9 +36,9 @@ const getCredentials = async () => {
     Key: `credentials/client/${ENV_FILE}`
   });
 
-  const data = await streamToString(response.Body)
+  const data = await streamToString(response.Body);
 
-  fs.writeFileSync("paladins-edge.env", data)
+  fs.writeFileSync("paladins-edge.env", data);
 };
 
-getCredentials()
+getCredentials();
