22d861783bebf292fdbbc2003fc3bb60aae45374
Tushar Jain <tusharlock10@gmail.com>
2024-02-28 04:40:54 +0530

    feat: add search for connect profile

--- lib/widgets/connect_player_modal.dart
+++ lib/widgets/connect_player_modal.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
+import "package:flutter_feather_icons/flutter_feather_icons.dart";
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:paladinsedge/api/index.dart" as api;
 import "package:paladinsedge/providers/index.dart" as providers;
 import "package:paladinsedge/theme/index.dart" as theme;
 import "package:paladinsedge/utilities/index.dart" as utilities;
@@ -43,19 +45,55 @@ void showConnectPlayerModal(BuildContext context) {
   );
 }
 
-class _ConnectPlayerSearch extends HookWidget {
-  final void Function(String) onSearch;
-
-  const _ConnectPlayerSearch({required this.onSearch});
-
+class _ConnectPlayerHeader extends HookConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Providers
+    final user = ref.watch(providers.auth.select((_) => _.user));
+
+    // Variables
+    final textTheme = Theme.of(context).textTheme;
+    final firstName = user?.name.split(" ").first;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Hi, $firstName",
+            style: textTheme.bodyLarge?.copyWith(fontSize: 18),
+          ),
+          Text(
+            "Link Paladins Edge to your profile, search your name and tap to connect",
+            style: textTheme.bodyLarge?.copyWith(fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ConnectPlayerSearch extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Providers
+    final playersProvider = ref.read(providers.players);
+    final lowerSearchList =
+        ref.watch(providers.players.select((_) => _.lowerSearchList));
+
     // Variables
     final isLightTheme = Theme.of(context).brightness == Brightness.light;
-
     final textTheme = Theme.of(context).textTheme;
     final textStyle = textTheme.bodyLarge?.copyWith(fontSize: 16);
 
+    // State
+    final isLoading = useState(false);
+
+    // Hooks
+    final textController = useTextEditingController();
+
+    // Methods
     final searchBarColor = useMemoized(
       () => isLightTheme
           ? theme.subtleLightThemeColor
@@ -63,45 +101,6 @@ class _ConnectPlayerSearch extends HookWidget {
       [isLightTheme],
     );
 
-    return TextField(
-      maxLength: 16,
-      enableInteractiveSelection: true,
-      style: textStyle?.copyWith(color: Colors.white),
-      onSubmitted: onSearch,
-      decoration: InputDecoration(
-        filled: true,
-        isDense: true,
-        hintText: "Enter your Paladins IGN",
-        counterText: "",
-        hintStyle: textStyle?.copyWith(color: Colors.white70),
-        fillColor: searchBarColor,
-        contentPadding: const EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 15,
-        ),
-        border: const OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-        ),
-      ),
-    );
-  }
-}
-
-class _ConnectPlayerModal extends HookConsumerWidget {
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // Providers
-    final playersProvider = ref.read(providers.players);
-    final user = ref.watch(providers.auth.select((_) => _.user));
-
-    // Variables
-    final textTheme = Theme.of(context).textTheme;
-
-    // State
-    final isLoading = useState(false);
-
-    // Methods
     final onNotFound = useCallback(
       (String playerName) {
         widgets.showToast(
@@ -113,14 +112,22 @@ class _ConnectPlayerModal extends HookConsumerWidget {
       [],
     );
 
+    final onClear = useCallback(
+      () {
+        textController.clear();
+        playersProvider.clearSearchList();
+      },
+      [],
+    );
+
     final onSearch = useCallback(
-      (String playerName) async {
+      () async {
         // exactMatch will always be false
         // topSearchList will be empty
         // lowerSearchList will contain all the search data
         // even for a single item
 
-        playerName = playerName.trim();
+        final playerName = textController.text.trim();
         if (playerName.isEmpty) return;
 
         isLoading.value = true;
@@ -134,32 +141,148 @@ class _ConnectPlayerModal extends HookConsumerWidget {
 
         isLoading.value = false;
       },
-      [],
+      [textController],
     );
 
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: textController,
+            maxLength: 20,
+            enableInteractiveSelection: true,
+            style: textStyle?.copyWith(color: Colors.white),
+            onSubmitted: (_) => onSearch(),
+            decoration: InputDecoration(
+              filled: true,
+              isDense: true,
+              hintText: "Enter your Paladins IGN",
+              counterText: "",
+              hintStyle: textStyle?.copyWith(color: Colors.white70),
+              fillColor: searchBarColor,
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 15,
+              ),
+              border: const OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 5),
+        SizedBox(
+          width: 36,
+          height: 36,
+          child: isLoading.value
+              ? const widgets.LoadingIndicator(
+                  size: 18,
+                  lineWidth: 2,
+                  center: true,
+                )
+              : IconButton(
+                  iconSize: 18,
+                  onPressed: () =>
+                      lowerSearchList.isEmpty ? onSearch() : onClear(),
+                  icon: Icon(
+                    lowerSearchList.isEmpty
+                        ? FeatherIcons.search
+                        : FeatherIcons.x,
+                  ),
+                ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ConnectPlayerList extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Providers
+    final lowerSearchList = ref.watch(
+      providers.players.select((_) => _.lowerSearchList),
+    );
+
+    // Variables
+    final textTheme = Theme.of(context).textTheme;
+
+    // State
+    final selectedPlayer = useState<api.LowerSearch?>(null);
+
+    // Effects
+    useEffect(
+      () {
+        selectedPlayer.value = null;
+
+        return null;
+      },
+      [selectedPlayer, lowerSearchList],
+    );
+
+    return Expanded(
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        child: selectedPlayer.value != null
+            ? Column(
+                children: [
+                  ListTile(
+                    onTap: () {},
+                    title: Text(selectedPlayer.value!.name),
+                    subtitle: Text(selectedPlayer.value!.platform),
+                    trailing: Text(selectedPlayer.value!.playerId),
+                    subtitleTextStyle: textTheme.bodyLarge?.copyWith(
+                      fontSize: 14,
+                    ),
+                  ),
+                  widgets.Button(
+                    label: "Link Profile",
+                    onPressed: () {},
+                  ),
+                ],
+              )
+            : ListView.separated(
+                itemCount: lowerSearchList.length,
+                separatorBuilder: (_, __) => const Divider(
+                  color: Colors.white,
+                  thickness: 1,
+                  height: 1,
+                  indent: 10,
+                ),
+                itemBuilder: (_, index) {
+                  final player = lowerSearchList[index];
+
+                  return ListTile(
+                    onTap: () => selectedPlayer.value = player,
+                    title: Text(player.name),
+                    subtitle: Text(player.platform),
+                    trailing: Text(player.playerId),
+                    subtitleTextStyle: textTheme.bodyLarge?.copyWith(
+                      fontSize: 14,
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
+  }
+}
+
+class _ConnectPlayerModal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
       child: Padding(
         padding: const EdgeInsets.all(15),
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 10, bottom: 20, right: 25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hi, ${user?.name}",
-                    style: textTheme.bodyLarge?.copyWith(fontSize: 18),
-                  ),
-                  Text(
-                    "Link Paladins Edge to your profile, search your name and tap to connect",
-                    style: textTheme.bodyLarge?.copyWith(fontSize: 14),
-                  ),
-                ],
-              ),
-            ),
-            _ConnectPlayerSearch(onSearch: onSearch),
+            _ConnectPlayerHeader(),
+            const SizedBox(height: 10),
+            _ConnectPlayerSearch(),
+            const SizedBox(height: 10),
+            _ConnectPlayerList(),
           ],
         ),
       ),
